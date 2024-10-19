21c714f2ef67b30d580914d05115435c738177a1
smialekma <smialekma@gmail.com>
2024-10-19 10:09:31 +0200

    feat(tests) add tests for decoders

---
+++ tests/decoders/test_rot_decoder.py
@@ -0,0 +1,32 @@
+class TestRot13DecoderDecode:
+    def test_should_return_decoded_words(self):
+        pass
+
+    def test_should_return_decoded_sentences(self):
+        pass
+
+    def test_should_return_numbers_when_numbers_are_called(self):
+        pass
+
+    def test_should_return_punctuation_without_decoding(self):
+        pass
+
+    def test_should_return_numbers_without_decoding(self):
+        pass
+
+
+class TestRot13DecoderEncode:
+    def test_should_return_encoded_words(self):
+        pass
+
+    def test_should_return_encoded_sentences(self):
+        pass
+
+    def should_return_numbers_when_numbers_are_called(self):
+        pass
+
+    def test_should_return_punctuation_without_encoding(self):
+        pass
+
+    def test_should_return_numbers_without_encoding(self):
+        pass
